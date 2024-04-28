3c524c6efd54cc1442dfa69d083bfcea0dc7e52d
Moldir <moldir.andirbekova@mail.ru>
2024-04-28 11:54:07 +0400

    imput form

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+def get_risk_level(probability):
+    if probability < 20:
+        return "extremely low"
+    elif probability < 40:
+        return "low"
+    elif probability < 60:
+        return "medium"
+    elif probability < 80:
+        return "high"
+    else:
+        return "extremely high"
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    probability = 10
+    risk_level = get_risk_level(probability)
+    return render_template('predict.html', risk_level=risk_level)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
